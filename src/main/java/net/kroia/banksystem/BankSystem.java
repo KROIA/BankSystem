f733bfe57e0b111521209ddf0b7a291813cffa07
Alex Krieg <alexkrieg@gmx.ch>
2024-12-22 17:03:47 +0100

    init

---
+++ src/main/java/net/kroia/banksystem/BankSystem.java
@@ -0,0 +1,78 @@
+package net.kroia.banksystem;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BankSystem.MODID)
+public class BankSystem
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "banksystem";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public BankSystem()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+
+
+            if (ModList.get().isLoaded("stockmarket"))
+            {
+                System.out.println("stockmarket is loaded");
+
+                /*
+                if(TestAPI.getTestInterface() != null)
+                    TestAPI.getTestInterface().test();
+                else
+                    System.out.println("TestInterface is null");*/
+            }
+            else
+            {
+                System.out.println("stockmarket is not loaded");
+            }
+        }
+    }
+}
---
+++ src/main/java/net/kroia/banksystem/banking/bank/ItemBank.java
@@ -0,0 +1,26 @@
+package net.kroia.banksystem.banking.bank;
+
+import net.kroia.banksystem.banking.BankUser;
+import net.minecraft.nbt.CompoundTag;
+
+public class ItemBank extends Bank {
+
+    public ItemBank(BankUser owner, String itemID, long balance) {
+        super(owner, itemID, balance);
+    }
+    public ItemBank(BankUser owner, CompoundTag tag) {
+        super(owner, tag);
+    }
+
+    @Override
+    public boolean save(CompoundTag tag) {
+        tag.putString("BankType", BankType.ITEM.name());
+        return super.save(tag);
+    }
+
+    @Override
+    public boolean load(CompoundTag tag) {
+        return super.load(tag);
+    }
+
+}
