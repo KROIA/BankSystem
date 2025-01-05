764256002111ff299aaf83a7e452b00438e9b281
Alex <alexkrieg@gmx.ch>
2025-01-05 20:32:33 +0100

    Downgrade to 1.19.3

--- common/src/main/java/net/kroia/banksystem/screen/custom/BankSystemSettingScreen.java
+++ common/src/main/java/net/kroia/banksystem/screen/custom/BankSystemSettingScreen.java
@@ -56,12 +56,6 @@ public class BankSystemSettingScreen extends GuiScreen {
             itemSelectionScreen.sortItems();
             this.minecraft.setScreen(itemSelectionScreen);
         });
-        //closeButton = new CloseButton(this::onClose);
-        closeButton.setIdleColor(0xFFf55a42);
-        closeButton.setHoverColor(0xFFe03d24);
-        closeButton.setPressedColor(0xFFde2b10);
-        closeButton.setOutlineColor(0xFFde2510);
-        closeButton.setSize(20,20);
 
         currentBankingItemsView = new ItemSelectionView(ClientBankManager.getAllowedItemIDs(), this::setCurrentBankingItemID);
         currentBankingItemsView.setPosition(padding, padding);
