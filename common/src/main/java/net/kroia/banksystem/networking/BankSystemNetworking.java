e41ef34bc3a4c2881522393c59790b0021a609c7
Alex <alexkrieg@gmx.ch>
2025-01-08 19:28:55 +0100

    ~Network S2C Packet Registration fix

--- common/src/main/java/net/kroia/banksystem/networking/BankSystemNetworking.java
+++ common/src/main/java/net/kroia/banksystem/networking/BankSystemNetworking.java
@@ -1,7 +1,9 @@
 package net.kroia.banksystem.networking;
 
-import dev.architectury.networking.simple.MessageType;
-import dev.architectury.networking.simple.SimpleNetworkManager;
+import dev.architectury.impl.NetworkAggregator;
+import dev.architectury.networking.simple.*;
+import dev.architectury.platform.Platform;
+import dev.architectury.utils.Env;
 import net.kroia.banksystem.BankSystemMod;
 import net.kroia.banksystem.networking.packet.client_sender.request.*;
 import net.kroia.banksystem.networking.packet.client_sender.update.UpdateBankAccountPacket;
@@ -11,26 +13,28 @@ import net.kroia.banksystem.networking.packet.server_sender.update.SyncBankDataP
 import net.kroia.banksystem.networking.packet.server_sender.update.SyncItemInfoPacket;
 import net.kroia.banksystem.networking.packet.server_sender.update.SyncPotentialBankItemIDsPacket;
 
+import java.util.List;
+
 
 public class BankSystemNetworking {
 
     public static final SimpleNetworkManager CHANNEL = SimpleNetworkManager.create(BankSystemMod.MOD_ID);
 
 
-    public static MessageType SYNC_BANK_DATA = CHANNEL.registerS2C(getClassName(SyncBankDataPacket.class.getName()), SyncBankDataPacket::new);
-    public static MessageType SYNC_POTENTIAL_BANK_ITEM_IDS = CHANNEL.registerS2C(getClassName(SyncPotentialBankItemIDsPacket.class.getName()), SyncPotentialBankItemIDsPacket::new);
-    public static MessageType SYNC_OPEN_GUI = CHANNEL.registerS2C(getClassName(SyncOpenGUIPacket.class.getName()), SyncOpenGUIPacket::new);
-    public static MessageType SYNC_ITEM_INFO = CHANNEL.registerS2C(getClassName(SyncItemInfoPacket.class.getName()), SyncItemInfoPacket::new);
+    public static MessageType SYNC_BANK_DATA = registerS2C(getClassName(SyncBankDataPacket.class.getName()), SyncBankDataPacket::new);
+    public static MessageType SYNC_POTENTIAL_BANK_ITEM_IDS = registerS2C(getClassName(SyncPotentialBankItemIDsPacket.class.getName()), SyncPotentialBankItemIDsPacket::new);
+    public static MessageType SYNC_OPEN_GUI = registerS2C(getClassName(SyncOpenGUIPacket.class.getName()), SyncOpenGUIPacket::new);
+    public static MessageType SYNC_ITEM_INFO = registerS2C(getClassName(SyncItemInfoPacket.class.getName()), SyncItemInfoPacket::new);
 
 
 
-    public static MessageType REQUEST_ALLOW_NEW_BANK_ITEM_ID = CHANNEL.registerC2S(getClassName(RequestAllowNewBankItemIDPacket.class.getName()), RequestAllowNewBankItemIDPacket::new);
-    public static MessageType REQUEST_BANK_DATA = CHANNEL.registerC2S(getClassName(RequestBankDataPacket.class.getName()), RequestBankDataPacket::new);
-    public static MessageType REQUEST_DISALLOW_BANKING_ITEM_ID = CHANNEL.registerC2S(getClassName(RequestDisallowBankingItemIDPacket.class.getName()), RequestDisallowBankingItemIDPacket::new);
-    public static MessageType REQUEST_POTENTIAL_BANK_ITEM_IDS = CHANNEL.registerC2S(getClassName(RequestPotentialBankItemIDsPacket.class.getName()), RequestPotentialBankItemIDsPacket::new);
-    public static MessageType UPDATE_BANK_ACCOUNT = CHANNEL.registerC2S(getClassName(UpdateBankAccountPacket.class.getName()), UpdateBankAccountPacket::new);
-    public static MessageType UPDATE_BANK_TERMINAL_BLOCK_ENTITY = CHANNEL.registerC2S(getClassName(UpdateBankTerminalBlockEntityPacket.class.getName()), UpdateBankTerminalBlockEntityPacket::new);
-    public static MessageType REQUEST_ITEM_INFO = CHANNEL.registerC2S(getClassName(RequestItemInfoPacket.class.getName()), RequestItemInfoPacket::new);
+    public static MessageType REQUEST_ALLOW_NEW_BANK_ITEM_ID = registerC2S(getClassName(RequestAllowNewBankItemIDPacket.class.getName()), RequestAllowNewBankItemIDPacket::new);
+    public static MessageType REQUEST_BANK_DATA = registerC2S(getClassName(RequestBankDataPacket.class.getName()), RequestBankDataPacket::new);
+    public static MessageType REQUEST_DISALLOW_BANKING_ITEM_ID = registerC2S(getClassName(RequestDisallowBankingItemIDPacket.class.getName()), RequestDisallowBankingItemIDPacket::new);
+    public static MessageType REQUEST_POTENTIAL_BANK_ITEM_IDS = registerC2S(getClassName(RequestPotentialBankItemIDsPacket.class.getName()), RequestPotentialBankItemIDsPacket::new);
+    public static MessageType UPDATE_BANK_ACCOUNT = registerC2S(getClassName(UpdateBankAccountPacket.class.getName()), UpdateBankAccountPacket::new);
+    public static MessageType UPDATE_BANK_TERMINAL_BLOCK_ENTITY = registerC2S(getClassName(UpdateBankTerminalBlockEntityPacket.class.getName()), UpdateBankTerminalBlockEntityPacket::new);
+    public static MessageType REQUEST_ITEM_INFO = registerC2S(getClassName(RequestItemInfoPacket.class.getName()), RequestItemInfoPacket::new);
 
 
 
@@ -40,5 +44,20 @@ public class BankSystemNetworking {
     private static String getClassName(String name) {
         String sub = name.substring(name.lastIndexOf(".")+1).toLowerCase();
         return sub;
+    }
+
+    public static MessageType registerS2C(String name, MessageDecoder<BaseS2CMessage> decoder)
+    {
+        MessageType registeredMsg = CHANNEL.registerS2C(name, decoder);
+        if (Platform.getEnvironment() == Env.SERVER)
+        {
+            NetworkAggregator.registerS2CType(registeredMsg.getId(), List.of());
+        }
+        return registeredMsg;
+    }
+    public static MessageType registerC2S(String name, MessageDecoder<BaseC2SMessage> decoder)
+    {
+        MessageType registeredMsg = CHANNEL.registerC2S(name, decoder);
+        return registeredMsg;
     }
 }
