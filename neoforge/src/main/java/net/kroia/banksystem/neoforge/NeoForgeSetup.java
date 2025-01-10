8b5c89f9efa0530648494f8152468118f9a1f88a
Alex <alexkrieg@gmx.ch>
2025-01-10 13:03:23 +0100

    -

--- neoforge/src/main/java/net/kroia/banksystem/neoforge/NeoForgeSetup.java
+++ neoforge/src/main/java/net/kroia/banksystem/neoforge/NeoForgeSetup.java
@@ -1,10 +1,13 @@
 package net.modutilities.neoforge;
 
 import net.kroia.banksystem.BankSystemMod;
+import net.kroia.banksystem.menu.BankSystemMenus;
+import net.kroia.banksystem.screen.custom.BankTerminalScreen;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
 @Mod.EventBusSubscriber(modid = BankSystemMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class NeoForgeSetup {
